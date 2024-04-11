68037aaad89a785970413078e4fa3f4f89bb0056
Godfrey <mpoly702@gmail.com>
2024-04-11 19:28:00 +0100

    n

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 
 	/* new_node(new node) initialization */
-	new_node->data = value;
+	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
